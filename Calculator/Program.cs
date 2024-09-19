f5a7e45589a639ed28b1533d768c488f21002c57
TuanKhang16 <kahnedy0106@gmail.com>
2024-09-19 14:49:50 +0700

    Them test

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -13,5 +13,16 @@ namespace Calculator
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            double a =
+            double.Parse(txtA.Text);
+            double.Parse(txtB.Text);
+            double b =
+            double c =
+            a - b;
+            txtKetQua.Text = c.ToString();
+        }
     }
 }
